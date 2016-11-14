07bf4e77b8981a488493f84a87e8a9c3eeb8af35
Yingyin <105791120@live.asia.edu.tw>
2016-11-14 16:55:48 +0800

    scr01 105791120 楊鈺瑩

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,4 +1,7 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Date: 2016/11/14
  * Author: 1050210XX OOO
@@ -6,7 +9,29 @@ package ex;
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sca = new Scanner(System.in);
+		int n=sca.nextInt();
+		int sum=0;
+		int data[]=new int [n];
+		for(int i=0;i<n;i++)
+		{
+			data[i]=sca.nextInt();
+			sum+=data[i];
+		}
+		sum=sum/n;
+		System.out.println(var(sum,data,n)/n);
+		}
+
+
+		public static float var(int x, int[] data,int n) {
+			int y;
+			float sum=0;
+			for(int i=0;i<n;i++)
+			{
+				y=x-(data[i]);
+				sum=(float)(sum+(Math.pow(y, 2)));
+			}
+			return sum;
 
 	}
 
