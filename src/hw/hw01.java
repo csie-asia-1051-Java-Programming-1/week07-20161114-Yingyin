241f90225938bcd2a99029350f15f49c3e164d1b
Yingyin <105791120@live.asia.edu.tw>
2016-11-20 17:12:37 +0800

    hw01 105791120 杨钰莹

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,30 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		int x,y,z;
+		x=scan.nextInt();
+		y=scan.nextInt();
+		z=scan.nextInt();
+		System.out.println(lcm(x, y, z));
+	}
+	public static int gcd(int x,int y){
+		int r;
+		while((r=x%y)!=0){
+			x=y;
+			y=r;
+		}
+		return y;
+	}
+	public static int lcm(int x,int y){
+		return x*y/gcd(x, y);
+	}
+	public static int lcm(int x,int y,int z){
+		return lcm(x, y)*z/gcd(lcm(x, y), z);
 	}
 
 }
