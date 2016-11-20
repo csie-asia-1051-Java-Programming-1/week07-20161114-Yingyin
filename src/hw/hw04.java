8727b3e613a4a59c028278fab83f00502a0783fd
Yingyin <105791120@live.asia.edu.tw>
2016-11-20 17:14:36 +0800

    hw04 105791120 杨钰莹

--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,10 +1,20 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		int m=scan.nextInt();
+		int n=scan.nextInt();
+		System.out.println(gcd(m, n));
 	}
-
+	public static int gcd(int m,int n){
+		if(m%n==0)
+			return n;
+		else {
+			return gcd(n, m%n);
+		}
+}
 }
