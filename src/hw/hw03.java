4b2309ffbb393f95d4a97fbfa2988487791ddb82
Yingyin <105791120@live.asia.edu.tw>
2016-11-20 17:13:58 +0800

    hw03 105791120 杨钰莹

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,10 +1,27 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sca=new Scanner(System.in);
+	int m=sca.nextInt();
+	int n=sca.nextInt();
+	System.out.println(C(m,n));
+	}
 
+	public static int C(int m, int n) {
+		return fun(m)/(fun(n)*(fun(m-n)));
+	}
+	public static int fun(int x)
+	{
+		if(x>1)
+		{
+			return x*fun(x-1);
+		}else{
+			return 1;
+		}
 	}
 
 }
