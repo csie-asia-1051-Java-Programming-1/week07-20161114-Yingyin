0a6ec11a387b368f04fd473eb0ef204a3e63c7c1
Yingyin <105791120@live.asia.edu.tw>
2016-11-20 17:14:48 +0800

    hw05 105791120 杨钰莹

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -1,10 +1,20 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		System.out.println(reStr(n,0));
 	}
+	public static int reStr(int n,int p){
+		p=p*10+(n%10);
+		if(n>=10){
+			return reStr(n/10, p);
+		}
+		return p;
+		}
 
 }
